03404251452a9bb3f307b865dbc6465d52cd0c00
onurkaymak <onurkaymak@outlook.com>
2023-10-28 22:09:47 -0700

    Apply AddForce to the Player<RigidBody>.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
 
+    private float movementX;
+
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +21,17 @@ public class PlayerController : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+
+        movementY = movementVector.y;
         
     }
 
     void FixedUpdate()
     {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
+        rb.AddForce(movement);
     }
 }
